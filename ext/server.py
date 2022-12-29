6018e3cb92b37c0b8a6f1f2ff02e406a63731ce3
Alice S <snapp@null.net>
2022-12-29 02:46:07 -0500

    edit syntax to use tmux instead of screen

--- ext/server.py
+++ ext/server.py
@@ -27,9 +27,9 @@ def get_active_screens() -> List[str]:
     return session_names
 
 async def until_quit():
-    output = ""
-    while "$" not in output:
-        result = subprocess.run(['screen', '-S', session_name, '-Q', 'windows'], stdout=subprocess.PIPE)
+    output = "server"
+    while "server" in output:
+        result = subprocess.run(['tmux', 'list-windows'], stdout=subprocess.PIPE)
         output = result.stdout.decode()
         print(output)
         await asyncio.sleep(1)
@@ -40,11 +40,10 @@ def start_screen():
     subprocess.run(['screen', '-d', '-m', '-S', session_name])
 
 def command(cmd, args):
-    os.system(f"screen -S {session_name} -p 0 -X {cmd} {args}")
+    os.system(f"tmux send -t {session_name}:1 {cmd} {args} ENTER")
 
 def send(*args):
-    command("stuff", f"{' '.join(args)}^M")
-    command("stuff", "^D")
+    command("stuff", f"{' '.join(args)}")
 
 def message(msg):
     send("servermsg", f'"{msg}"')
@@ -53,7 +52,7 @@ def quit():
     send('quit')
 
 def start():
-    command("exec bash", "/opt/pzserver/start-server.sh")
+    subprocess.run(['tmux', 'new-window', '-t', f'{session_name}:1', '-n', 'server', '"bash /opt/pzserver/start-server.sh"'])
 
 async def restart():
     quit()
