72dd7bb32b440bfefe0837073c08faba0e7c7b1a
Alice S <snapp@null.net>
2022-12-29 02:46:26 -0500

    change process name to 'zomboid'

--- ext/server.py
+++ ext/server.py
@@ -4,27 +4,7 @@ import os, sys
 from typing import List
 import subprocess
 
-session_name = 'pz'
-
-def get_active_screens() -> List[str]:
-    result = subprocess.run(['screen', '-ls'], stdout=subprocess.PIPE)
-    output = result.stdout.decode()
-    if "No sockets found" in output:
-        return []
-    assert "There is a screen on" in output
-
-    lines = []
-    for line in output.splitlines():
-        if line.startswith('\t'):
-            lines.append(line)
-
-    session_names = []
-    for line in lines:
-        line = line.strip()
-        line = line.split('\t')[0]
-        line = line.split('.')[1]
-        session_names.append(line)
-    return session_names
+session_name = 'zomboid'
 
 async def until_quit():
     output = "server"
@@ -35,9 +15,6 @@ async def until_quit():
         await asyncio.sleep(1)
     else:
         print("Window closed")
-
-def start_screen():
-    subprocess.run(['screen', '-d', '-m', '-S', session_name])
 
 def command(cmd, args):
     os.system(f"tmux send -t {session_name}:1 {cmd} {args} ENTER")
