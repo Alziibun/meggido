111eac43b25ce1c7dc4a946abab8faaf0b9b3ce6
Alice S <snapp@null.net>
2022-12-30 18:09:42 -0500

    i'm new to decorators

--- ext/database.py
+++ ext/database.py
@@ -4,13 +4,13 @@ server_name = 'servertest'
 engine =  sql.create_engine(f"sqlite+pysqlite:////~/Zomboid/db/{server_name}.db", echo=True, future=True)
 
 def connect(f):
-    def wrapper():
+    def wrapper(*args, **kwargs):
         with engine.connect() as connection:
-            f(con=connection)
+            f(con=connection, *args, **kwargs)
     return wrapper
 
 @connect
-def get_user(con: sql.engine.Engine, name: str=""):
+def get_user(con: sql.engine.Engine, *, name: str=""):
     result = con.execute(sql.text(f'SELECT * FROM whitelist WHERE username = {name}'))
     return result.all()[0]  # return the first result
 
